4bdb4643a61e724a992e071aa173d24a25da3042
David Shea <dshea@redhat.com>
2014-02-08 10:58:14 +0100

    Created an anaconda addon to configure kdump

---
+++ com_redhat_kdump/common.py
@@ -0,0 +1,78 @@
+# Kdump configuration common methods
+#
+# Copyright (C) 2014 Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+# Red Hat Author(s): David Shea <dshea@redhat.com>
+#
+
+__all__ = ["getReservedMemory", "getTotalMemory", "getMemoryBounds"]
+
+from pyanaconda.isys import total_memory
+from pyanaconda.flags import flags
+
+_reservedMemory = None
+def getReservedMemory():
+    """Return the amount of memory currently reserved for kdump in MB."""
+    global _reservedMemory
+
+    # Check if the value has already been read
+    if _reservedMemory is not None:
+        return _reservedMemory
+
+    try:
+        with open("/sys/kernel/kexec_crash_size", "r") as fobj:
+            _reservedMemory = int(fobj.read()) / (1024*1024)
+            return _reservedMemory
+    except (ValueError, IOError):
+        return 0
+
+def getTotalMemory():
+    """Return the total amount of system memory in MB
+
+       This is the amount reported by /proc/meminfo plus the aount
+       currently reserved for kdump.
+    """
+
+    # total_memory return memory in KB, convert to MB
+    availMem = total_memory() / 1024
+
+    return availMem + getReservedMemory()
+
+def getMemoryBounds():
+    """Return a tuple of (lower, upper, step) for kdump reservation limits.
+
+       If there is not enough memory available to use kdump, both lower and
+       upper will be 0.
+    """
+
+    totalMemory = getTotalMemory()
+
+    if flags.targetarch == 'ppc64':
+        lowerBound = 256
+        minUsable = 1024
+        step = 1
+    else:
+        lowerBound = 128
+        minUsable = 256
+        step = 1
+
+    upperBound = (totalMemory - minUsable) - (totalMemory % step)
+
+    if upperBound < lowerBound:
+        upperBound = lowerBound = 0
+
+    return (lowerBound, upperBound, step)
---
+++ com_redhat_kdump/tui/spokes/kdump.py
@@ -0,0 +1,73 @@
+# Kdump anaconda configuration
+#
+# Copyright (C) 2013 Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+# Red Hat Author(s): David Shea <dshea@redhat.com>
+#
+
+"""Kdump anaconda TUI configuration"""
+
+import re
+
+from pyanaconda.ui.tui.spokes import EditTUISpoke
+from pyanaconda.ui.tui.spokes import EditTUISpokeEntry as Entry
+
+from com_redhat_kdump.i18n import N_, _
+from com_redhat_kdump.constants import CONFIG_FILE
+
+__all__ = ["KdumpSpoke"]
+
+# Allow either "auto" or a string of digits optionally followed by 'M'
+RESERVE_VALID = re.compile(r'^((auto)|(\d+M?))$')
+
+class KdumpSpoke(EditTUISpoke):
+    title = N_("Kdump")
+    category = "system"
+
+    edit_fields = [
+        Entry("Enable kdump", "enabled", EditTUISpoke.CHECK, True),
+        Entry("Reserve amount", "reserveMB", RESERVE_VALID, lambda self,args: args.enabled)
+        ]
+
+    def __init__(self, app, data, storage, payload, instclass):
+        EditTUISpoke.__init__(self, app, data, storage, payload, instclass)
+
+        self.args = self.data.addons.com_redhat_kdump
+
+        # Read the config file into data.content so that it will be written
+        # to the system even though it is not editable
+        try:
+            with open(CONFIG_FILE, "r") as fobj:
+                self.data.addons.com_redhat_kdump.content = fobj.read()
+        except IOError:
+            pass
+
+    def apply(self):
+        pass
+
+    @property
+    def completed(self):
+        return True
+
+    @property
+    def status(self):
+        if self.args.enabled:
+            state = _("Kdump is enabled")
+        else:
+            state = _("Kdump is disabled")
+
+        return state
