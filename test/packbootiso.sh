0fae4a0d5aad2356430c2dfde51830a366cdd265
Dave Young <dyoung@redhat.com>
2014-07-24 15:51:27 +0800

    packbootiso.sh improvement and fixes
    1. the boot.iso and kdump addon tarball was hardcoded previously, fix to
    use arguments instead
    2. use iso-info to extract iso Volume label
    3. Indent issues.

--- test/packbootiso.sh
+++ test/packbootiso.sh
@@ -3,16 +3,18 @@
 if [ -z "$1" ] || [ -z "$2" ]; then
 	echo "Usage:"
 	echo "	packbootiso.sh <boot.iso> <kdump_addon_tarball>"
-	echo "  # make sure you have mkisofs and mksquashfs tools installed"
+	echo "	# make sure you have mkisofs, iso-info and mksquashfs installed"
 	exit 1
 fi 
 
 boot_iso=$1
 addon_tarball=$2
+label=$(iso-info $boot_iso |grep Volume|cut -d':' -f2)
+label=$(echo $label)
 
 mkdir mnt
 
-mount -o loop boot.iso mnt
+mount -o loop $boot_iso mnt
 cp -ar mnt isodir
 umount mnt
 
@@ -21,7 +23,7 @@ cp -ar mnt squashdir
 umount mnt
 
 mount -o loop squashdir/LiveOS/rootfs.img mnt
-tar -xvzf kdump-*gz
+tar -xvzf $addon_tarball 
 cp -aRf kdump-anaconda-addon/com_redhat_kdump mnt/usr/share/anaconda/addons/
 make -C kdump-anaconda-addon/po install DESTDIR=../../mnt
 umount mnt
@@ -34,7 +36,7 @@ cd ..
 rm -rf squashdir
 
 cd isodir
-mkisofs -o ../boot-1.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J -V "Fedora-21-x86_64" -r .
+mkisofs -o ../boot-1.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J -V "$label" -r .
 cd ..
 
 rm -rf isodir mnt
