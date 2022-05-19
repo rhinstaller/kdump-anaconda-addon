rhinstaller/kdump-anaconda-addon

commit f3ea7ad97315acc07a855125e922a6f8a2c22af7

Warn the user when encrypted storage is in-use
Backport of following commit, with following up fixes squashed, some
code are rewritten due to differences between Fedora and RHEL 8:

commit d046d22af07c2613417ce31c27952f771c12922b
Author: Kairui Song <kasong@redhat.com>
Date:   Mon Jul 5 00:48:57 2021 +0800

    Warn the user when encrypted storage is in-use

    Currently kdump doesn't work well with encrypted storage, and it's hard
    to workaround the issue automatically. So warn the user for the potential
    issues.

    Signed-off-by: Kairui Song <kasong@redhat.com>

Signed-off-by: Kairui Song <kasong@redhat.com>

com_redhat_kdump/gui/spokes/kdump.py
@@ -24,14 +24,18 @@
 import os.path
 from gi.repository import Gtk
 
+from pyanaconda.flags import flags
+from pyanaconda.modules.common.constants.services import STORAGE
 from pyanaconda.core.kernel import kernel_arguments
 from pyanaconda.ui.categories.system import SystemCategory
 from pyanaconda.ui.gui.spokes import NormalSpoke
 from pyanaconda.ui.gui.utils import fancy_set_sensitive
+from pyanaconda.ui.communication import hubQ
 
 from com_redhat_kdump.i18n import _, N_
-from com_redhat_kdump.common import getTotalMemory, getMemoryBounds
-from com_redhat_kdump.constants import FADUMP_CAPABLE_FILE
+from com_redhat_kdump.constants import FADUMP_CAPABLE_FILE, ENCRYPTION_WARNING
+from com_redhat_kdump.common import getTotalMemory, getMemoryBounds, getLuksDevices
+
 
 __all__ = ["KdumpSpoke"]
 
@@ -56,6 +60,9 @@ class KdumpSpoke(NormalSpoke):
     def __init__(self, *args):
         NormalSpoke.__init__(self, *args)
         self._reserveMem = 0
+        self._ready = True
+        self._luks_devs = []
+        self._checked_luks_devs = []
 
     def initialize(self):
         NormalSpoke.initialize(self)
@@ -80,6 +87,10 @@ class KdumpSpoke(NormalSpoke):
         adjustment = Gtk.Adjustment(lower, lower, upper, step, step, 0)
         self._toBeReservedSpin.set_adjustment(adjustment)
         self._toBeReservedSpin.set_value(lower)
+
+        # Connect a callback to the PropertiesChanged signal.
+        storage = STORAGE.get_proxy()
+        storage.PropertiesChanged.connect(self._check_storage_change)
 
     def refresh(self):
         # If a reserve amount is requested, set it in the spin button
@@ -117,6 +128,10 @@ class KdumpSpoke(NormalSpoke):
         # Force a toggled signal on the button in case it's state has not changed
         self._enableButton.emit("toggled")
 
+        self.clear_info()
+        if self._luks_devs:
+            self.set_warning(_(ENCRYPTION_WARNING))
+
     def apply(self):
         # Copy the GUI state into the AddonData object
         self.data.addons.com_redhat_kdump.enabled = self._enableButton.get_active()
@@ -127,27 +142,47 @@ class KdumpSpoke(NormalSpoke):
         self.data.addons.com_redhat_kdump.reserveMB = reserveMem
         self.data.addons.com_redhat_kdump.enablefadump = self._fadumpButton.get_active()
 
+        # This hub have been visited, use should now be aware of the crypted devices issue
+        self._checked_luks_devs = self._luks_devs
+
+    def _check_storage_change(self, interface, changed, invalid):
+        self._ready = False
+        # pylint: disable=no-member
+        hubQ.send_not_ready(self.__class__.__name__)
+
+        partition = changed.get("AppliedPartitioning")
+        if partition:
+            self._luks_devs = getLuksDevices(partition.unpack())
+
+        self._ready = True
+        # pylint: disable=no-member
+        hubQ.send_ready(self.__class__.__name__, True)
+
     @property
     def ready(self):
-        return True
+        return self._ready
 
     @property
     def completed(self):
-        # Always treat as completed
+        """ Make sure user have checked the warning about crypted devices """
+        if self._luks_devs and self._checked_luks_devs != self._luks_devs and not flags.automatedInstall:
+            return False
         return True
 
     @property
     def mandatory(self):
-        return False
+        if self.data.addons.com_redhat_kdump.enabled:
+            return True
 
     @property
     def status(self):
-        if self.data.addons.com_redhat_kdump.enabled:
-            state = _("Kdump is enabled")
-        else:
-            state = _("Kdump is disabled")
-
-        return state
+        if not self.data.addons.com_redhat_kdump.enabled:
+            return _("Kdump is disabled")
+        if not self._ready:
+            return _("Checking storage...")
+        if self._luks_devs:
+            return _("Kdump may require extra setup for encrypted devices.")
+        return _("Kdump is enabled")
 
     # SIGNAL HANDLERS
     def on_enable_kdump_toggled(self, checkbutton, user_data=None):

com_redhat_kdump/tui/spokes/kdump.py
@@ -24,16 +24,17 @@
 import os.path
 import re
 
+from pyanaconda.modules.common.constants.services import STORAGE
 from pyanaconda.core.kernel import kernel_arguments
 from pyanaconda.ui.categories.system import SystemCategory
 from pyanaconda.ui.tui.spokes import NormalTUISpoke
 from pyanaconda.ui.tui.tuiobject import Dialog
-from simpleline.render.widgets import CheckboxWidget, EntryWidget
+from simpleline.render.widgets import CheckboxWidget, EntryWidget, TextWidget
 from simpleline.render.containers import ListColumnContainer
 from simpleline.render.screen import InputState
-from com_redhat_kdump.common import getMemoryBounds
+from com_redhat_kdump.common import getMemoryBounds, getLuksDevices
 from com_redhat_kdump.i18n import N_, _
-from com_redhat_kdump.constants import FADUMP_CAPABLE_FILE
+from com_redhat_kdump.constants import FADUMP_CAPABLE_FILE, ENCRYPTION_WARNING
 
 __all__ = ["KdumpSpoke"]
 
@@ -52,6 +53,25 @@ class KdumpSpoke(NormalTUISpoke):
 
         self._container = None
 
+        self._luks_devs = []
+        self._ready = True
+
+    def _check_storage_change(self, interface, changed, invalid):
+        self._ready = False
+        partition = changed.get("AppliedPartitioning")
+        if partition:
+            self._luks_devs = getLuksDevices(partition.unpack())
+        self._ready = True
+
+    @property
+    def ready(self):
+        return self._ready
+
+    def initialize(self):
+        # Connect a callback to the PropertiesChanged signal.
+        storage = STORAGE.get_proxy()
+        storage.PropertiesChanged.connect(self._check_storage_change)
+
     @classmethod
     def should_run(cls, environment, data):
         # the KdumpSpoke should run only if requested
@@ -66,11 +86,13 @@ class KdumpSpoke(NormalTUISpoke):
 
     @property
     def status(self):
-        if self._addon_data.enabled:
-            state = _("Kdump is enabled")
-        else:
-            state = _("Kdump is disabled")
-        return state
+        if not self.data.addons.com_redhat_kdump.enabled:
+            return _("Kdump is disabled")
+        if not self._ready:
+            return _("Checking storage...")
+        if self._luks_devs:
+            return _("Kdump may require extra setup for encrypted devices.")
+        return _("Kdump is enabled")
 
     def refresh(self, args=None):
         super().refresh(args)
@@ -83,6 +105,11 @@ class KdumpSpoke(NormalTUISpoke):
         if self._addon_data.enabled:
             self._create_fadump_checkbox()
             self._create_reserve_amount_text_widget()
+
+        if self._luks_devs:
+            self.window.add_separator()
+            message = TextWidget(_(ENCRYPTION_WARNING))
+            self.window.add(message)
 
         self.window.add_separator()
 

com_redhat_kdump/common.py
@@ -19,9 +19,11 @@
 # Red Hat Author(s): David Shea <dshea@redhat.com>
 #
 import re
-__all__ = ["getReservedMemory", "getTotalMemory", "getMemoryBounds"]
+__all__ = ["getReservedMemory", "getTotalMemory", "getMemoryBounds", "getLuksDevices"]
 
 import blivet.arch
+from pyanaconda.modules.common.structures.storage import DeviceData
+from pyanaconda.modules.common.constants.services import STORAGE
 
 _reservedMemory = None
 def getReservedMemory():
@@ -85,3 +87,20 @@ def getMemoryBounds():
         upperBound = lowerBound = 0
 
     return (lowerBound, upperBound, step)
+
+def getLuksDevices(object_path):
+    devs = []
+
+    partitioning = STORAGE.get_proxy(object_path)
+    device_tree = STORAGE.get_proxy(partitioning.GetDeviceTree())
+    devices = device_tree.GetDevices()
+
+    for device_name in devices:
+        device_data = DeviceData.from_structure(
+            device_tree.GetDeviceData(device_name)
+        )
+
+        if device_data.type == 'luks/dm-crypt':
+            devs.append(device_name)
+
+    return devs

com_redhat_kdump/constants.py
@@ -19,5 +19,9 @@
 # Red Hat Author(s): David Shea <dshea@redhat.com>
 #
 
-# The constants 
+# The constants
 FADUMP_CAPABLE_FILE = "/proc/device-tree/rtas/ibm,configure-kernel-dump"
+
+# To mark ENCRYPTION_WARNING as translatable
+_ = lambda x: x
+ENCRYPTION_WARNING = _('Encrypted storage is in use, using an encrypted device as dump target for kdump might fail. Please verify if kdump is working properly after the installation finished. For more details see the "Notes on encrypted dump target" section in /usr/share/doc/kexec-tools/kexec-kdump-howto.txt.')

commit ffd365e8b1885b6f7dd285685f3b94ac0bc83e52

return False instead of None for the mandatory property
Recently, it's found on RHEL8 that anaconda crashes when in the
installation GUI firstly disable kdump then enable encryption. This
happens because when kdump is disabled, the mandatory property returns
None instead of False. Fix it by return False when kdump is disabled.

Reported-by: c.stackpole@gmail.com
Reported-by: Suraj Raskar <sraskar@redhat.com>
Suggested-by: Vendula Poncova <vponcova@redhat.com>
Fixes: f3ea7ad ("Warn the user when encrypted storage is in-use")
Signed-off-by: Coiby Xu <coiby.xu@gmail.com>

com_redhat_kdump/gui/spokes/kdump.py
@@ -171,8 +171,10 @@ class KdumpSpoke(NormalSpoke):
 
     @property
     def mandatory(self):
+        # this function should always return boolean value
         if self.data.addons.com_redhat_kdump.enabled:
             return True
+        return False
 
     @property
     def status(self):